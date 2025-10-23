0a58e969c8932407f243c0a5fd4e64ebfa5fe636
RaulRMello <raulmello220@hotmail.com>
2025-10-23 16:35:40 -0300

    capturando o valor digitado no campo de input

--- script.js
+++ script.js
@@ -2,5 +2,5 @@ const input = document.getElementById("item")
 const button = document.getElementById("adiciona-item")
 
 button.addEventListener("click",(event) =>{
-
+    const newItem = input.value.trim()
 })
