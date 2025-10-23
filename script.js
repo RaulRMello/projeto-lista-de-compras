526837f8495277a1046330aa93f5f78d866e51ad
RaulRMello <raulmello220@hotmail.com>
2025-10-23 16:42:55 -0300

    Verificando se o campo está vazio

--- script.js
+++ script.js
@@ -3,4 +3,7 @@ const button = document.getElementById("adiciona-item")
 
 button.addEventListener("click",(event) =>{
     const newItem = input.value.trim()
+    if(newItem === ""){
+        alert("Por favor, digite um item antes de adicionar")
+    }
 })
