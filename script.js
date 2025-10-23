bcd9d1c764cc0690c223673c2025b7db9415f398
RaulRMello <raulmello220@hotmail.com>
2025-10-23 16:25:43 -0300

    obtendo os elementos para adicionar items

---
+++ script.js
@@ -0,0 +1,6 @@
+const input = document.getElementById("item")
+const button = document.getElementById("adiciona-item")
+
+button.addEventListener("click",(event) =>{
+    const newItem = input.value.trim()
+})
